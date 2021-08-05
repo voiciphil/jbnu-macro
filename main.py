4627a4b3eea0a318046be2e21ac25e7135374c86
voiciphil <voiciphil@gmail.com>
2021-08-06 01:36:40 +0900

    feat: 입력값을 명령행 인자로 변경

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import argparse
 from macro import Macro
 
-stu_no = input('stu_no: ')
-pw = input('pw: ')
-grade = int(input('grade: '))
-index = int(input('index: '))
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', type=str, help='학번')
+parser.add_argument('-p', type=str, help='비밀번호')
+parser.add_argument('-g', type=int, help='학년')
+parser.add_argument('-i', type=int, help='과목 인덱스')
+
+args = parser.parse_args()
+stu_no = args.n
+pw = args.p
+grade = args.g
+index = args.i
 
 flag = True
 while flag:
