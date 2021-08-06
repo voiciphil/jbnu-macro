ace86eebbba3d0ba2214347d6fed75eee9ba1d85
Sabro98 <inhyuk04@gmail.com>
2021-08-06 16:20:24 +0900

    feat: basket register

--- macro.py
+++ macro.py
@@ -7,15 +7,20 @@ from selenium.common.exceptions import ElementNotInteractableException
 
 
 class Macro:
-    def __init__(self, stu_no, pw, grade, index):
+    def __init__(self, stu_no, pw, grade, index, method):
         self.__driver = webdriver.Chrome(os.path.join(os.getcwd(), 'chromedriver'))
         self.__grade = grade
         self.__index = index
         self.__stu_no = stu_no
         self.__pw = pw
+        self.__method = method
         self.__LOOP_CNT = 500
 
     def run(self):
+        if not self.__is_valid_params():
+            print("params Error")
+            return False
+
         self.__open_browser()
         self.__login()
         self.__enter_registration_page()
@@ -26,6 +31,15 @@ class Macro:
         else:
             self.close_browser()
             return True
+    
+    # 유효성 검증
+    # 전공 선택, grade 없으면 오류
+    def __is_valid_params(self):
+        if self.__is_regist_major():
+            if not self.__grade:
+                return False
+
+        return True
 
     def __open_browser(self):
         self.__driver.implicitly_wait(3)
@@ -41,11 +55,14 @@ class Macro:
         login_xpath = '//*[@id="mainframe_VFrameSet_LoginFrame_form_div_login_div_form_btn_login"]/div'
         login = self.__driver.find_element_by_xpath(login_xpath)
 
-        stu_no.click()
-        stu_no.send_keys(self.__stu_no)
-        pw.click()
-        pw.send_keys(self.__pw)
-        login.click()
+        try:
+            stu_no.click()
+            stu_no.send_keys(self.__stu_no)
+            pw.click()
+            pw.send_keys(self.__pw)
+            login.click()
+        except AttributeError:  # 가끔 일어나는 js 오류 처리 -> 자세한 확인 필요함
+            return True
 
     def __enter_registration_page(self):
         sugang_xpath = '//*[@id="mainframe_VFrameSet_TopFrame_form_div_top_mnu_topmenu_0001TextBoxElement"]/div'
@@ -88,10 +105,15 @@ class Macro:
                          {'id': 'mainframe_VFrameSet_TopFrame_COM_CHECK_form_div_pattern_sta_codeTextBoxElement'})
         return divs.div.text
 
+    # 전공 신청 여부 함수
+    def __is_regist_major(self):
+        return self.__method == 0
+
     def __has_remaining_seat(self):
+        # 전공 / 장바구니 신청에 맞는 spinner
         spinner1, spinner2 = self.__set_spinner()
 
-        for i in range(self.__LOOP_CNT):
+        for _ in range(self.__LOOP_CNT):
             if self.__get_remaining_seat() > 0:
                 return True
             self.__refresh(spinner1, spinner2)
@@ -100,6 +122,13 @@ class Macro:
         return False
 
     def __set_spinner(self):
+        if self.__is_regist_major():
+            return self.__set_spinner_major()
+        if not self.__is_regist_major():
+            return self.__set_spinner_basket()
+
+    # 전공 신청할 때 spinner
+    def __set_spinner_major(self):
         spinner1_xpath = '//*[@id="mainframe_VFrameSet_WorkFrame_form_div_work_div_search_cbo_shyr"]/div'
         spinner1 = self.__driver.find_element_by_xpath(spinner1_xpath)
         spinner1.click()
@@ -112,6 +141,20 @@ class Macro:
 
         return spinner1, spinner2
 
+    # 장바구니 신청할 때 spinner (btns)
+    # 장바구니 신청할 때는 장바구니 btn -> (과목이 적은) 군사학 btn -> 장바구니 btn -> 군사학 -> ... 반복하면서 확인
+    def __set_spinner_basket(self):
+        # spinner1 -> 장바구니 btn
+        basket_id = 'mainframe_VFrameSet_WorkFrame_form_div_work_btn_rsrvCourTextBoxElement'
+        basket_btn = self.__driver.find_element_by_id(basket_id)
+
+        # spinner2 -> 군사학 btn
+        other_id = 'mainframe_VFrameSet_WorkFrame_form_div_work_btn_milTextBoxElement'
+        other_btn = self.__driver.find_element_by_id(other_id)
+
+        basket_btn.click()
+        return basket_btn, other_btn
+
     def __get_remaining_seat(self):
         current_id = 'mainframe_VFrameSet_WorkFrame_form_div_work_grd_gwam_body_gridrow_' + str(self.__index) + \
                      '_cell_' + str(self.__index) + '_7GridCellTextSimpleContainerElement'
@@ -123,13 +166,26 @@ class Macro:
 
         return int(total.text) - int(current.text)
 
+    # method에 따른 refresh를 내부에서 다르게 실행
     def __refresh(self, spinner1, spinner2):
+        if self.__is_regist_major():
+            self.__refresh_major(spinner1, spinner2)
+        if not self.__is_regist_major():
+            self.__refresh_basket(basket_btn=spinner1, other_btn=spinner2)
+
+    def __refresh_major(self, spinner1, spinner2):
         spinner1.click()
         spinner2.send_keys(Keys.DOWN)
         spinner2.send_keys(Keys.RETURN)
         spinner1.click()
         spinner2.send_keys(Keys.UP)
         spinner2.send_keys(Keys.RETURN)
+
+    # 장바구니 신청 시 버튼을 번갈아가면서 클릭
+    def __refresh_basket(self, basket_btn, other_btn):
+        other_btn.click()
+        sleep(0.25)
+        basket_btn.click()
 
     def __register(self):
         btn_id = 'mainframe_VFrameSet_WorkFrame_form_div_work_grd_gwam_body_gridrow_' + str(self.__index) + \
--- main.py
+++ main.py
@@ -4,18 +4,20 @@ from macro import Macro
 parser = argparse.ArgumentParser()
 parser.add_argument('-n', type=str, help='학번', required=True)
 parser.add_argument('-p', type=str, help='비밀번호', required=True)
-parser.add_argument('-g', type=int, help='학년', required=True)
 parser.add_argument('-i', type=int, help='과목 인덱스', required=True)
+parser.add_argument('-m', type=int, help='0 -> 전공, 1 -> 장바구니', required=True)
+parser.add_argument('-g', type=int, help='학년')
 
 args = parser.parse_args()
 stu_no = args.n
 pw = args.p
-grade = args.g
 index = args.i
+grade = args.g
+method = args.m
 
 flag = True
 while flag:
-    macro = Macro(stu_no, pw, grade, index)
+    macro = Macro(stu_no, pw, grade, index, method=method)
     try:
         flag = macro.run()
     except:
